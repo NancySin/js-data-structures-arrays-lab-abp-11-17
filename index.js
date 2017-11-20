8964d41424844488c13e19827579d2e922457c4d
nancy sin <nsin@sbcglobal.net>
2017-11-20 20:45:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const drivers=["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(){
+  drivers.push('Ralph');
+  console.log(drivers)
+}
+function destructivelyPrependDriver() {
+  drivers.unshift("Bob");
+  console.log(drivers)
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+  console.log(drivers)
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+  console.log(drivers)
+}
+
+function appendDriver(name) {
+  var newDrivers = [...drivers, "Broom"];
+   return(newDrivers)
+  console.log(drivers)
+}
+
+
+function removeLastDriver(){
+  drivers.slice(0, drivers.length - 1);
+  return (newdrivers)
+  console.log(drivers)
+}
